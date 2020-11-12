71319b5a49bb364b2039a99ab41940bfec1b851f
wdobry <wdobry@gmail.com>
2020-11-13 02:58:20 +0800

    manifest

--- src/views/sketches/particles/Sketch.js
+++ src/views/sketches/particles/Sketch.js
@@ -2,30 +2,6 @@ import React, { useRef } from "react";
 import { Canvas, useFrame } from "react-three-fiber";
 import * as THREE from "three";
 
-// const Box = ({ position }) => {
-//   // This reference will give us direct access to the mesh
-//   const mesh = useRef();
-//   // Set up state for the hovered and active state
-//   const [hovered, setHover] = useState(false);
-//   const [active, setActive] = useState(false);
-//   // Rotate mesh every frame, this is outside of React without overhead
-//   useFrame(() => {
-//     mesh.current.rotation.x = mesh.current.rotation.y += 0.03;
-//   });
-//   return (
-//     <mesh
-//       position={position}
-//       ref={mesh}
-//       scale={active ? [1.5, 1.5, 1.5] : [1, 1, 1]}
-//       onClick={(e) => setActive(!active)}
-//       onPointerOver={(e) => setHover(true)}
-//       onPointerOut={(e) => setHover(false)}
-//     >
-//       <boxBufferGeometry args={[1, 1, 1]} />
-//       <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
-//     </mesh>
-//   );
-// };
 const Plane = ({ position }) => {
   // This reference will give us direct access to the mesh
   const mesh = useRef();
